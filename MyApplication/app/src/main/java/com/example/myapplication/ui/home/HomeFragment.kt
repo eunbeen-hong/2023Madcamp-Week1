8fc142eb68d49177b1503df414686906949c8250
frica12 <fricaa@naver.com>
2023-06-30 17:17:26 +0900

    연ë연락처 추가 기능 구현

--- MyApplication/app/src/main/java/com/example/myapplication/ui/home/HomeFragment.kt
+++ MyApplication/app/src/main/java/com/example/myapplication/ui/home/HomeFragment.kt
@@ -1,43 +1,52 @@
 package com.example.myapplication.ui.home
 
+import android.app.AlertDialog
+import android.content.Context
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import android.widget.ArrayAdapter
-import android.widget.ListView
+import android.widget.*
 import androidx.fragment.app.Fragment
-import androidx.lifecycle.ViewModelProvider
+import com.example.myapplication.R
 import com.example.myapplication.databinding.FragmentHomeBinding
 import com.google.gson.Gson
 import com.google.gson.reflect.TypeToken
+import java.io.*
 
-data class Person(val name: String, val number: String)
+data class Person(val name: String, val number: String, val email: String, val instagram: String, val github: String)
 
 class HomeFragment : Fragment() {
 
     private var _binding: FragmentHomeBinding? = null
     private val binding get() = _binding!!
 
+    private val gson = Gson()
+
     override fun onCreateView(
         inflater: LayoutInflater,
         container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View {
-        val homeViewModel = ViewModelProvider(this).get(HomeViewModel::class.java)
         _binding = FragmentHomeBinding.inflate(inflater, container, false)
         val root: View = binding.root
 
-        homeViewModel.text.observe(viewLifecycleOwner) {
-        }
+        val fileName = "numbers.json"
+        copyAssetToFile(requireContext(), fileName)
 
-        val jsonFileString = context?.assets?.open("numbers.json")?.bufferedReader().use { it?.readText() }
-        val gson = Gson()
-        val people: List<Person> = gson.fromJson(jsonFileString, object : TypeToken<List<Person>>() {}.type)
-
+        val people = readFromFile(fileName)
         val listView: ListView = binding.listView
         val adapter = ArrayAdapter(requireContext(), android.R.layout.simple_list_item_1, people.map { "${it.name} : ${it.number}" })
         listView.adapter = adapter
+
+        listView.onItemClickListener = AdapterView.OnItemClickListener { _, _, position, _ ->
+            val person = people[position]
+            showDetailsDialog(person)
+        }
+
+        binding.addContact.setOnClickListener {
+            showAddContactDialog(fileName, adapter)
+        }
 
         return root
     }
@@ -46,4 +55,88 @@ class HomeFragment : Fragment() {
         super.onDestroyView()
         _binding = null
     }
+
+    private fun copyAssetToFile(context: Context, fileName: String) {
+        val file = File(context.filesDir, fileName)
+        if (file.exists()) {
+            return
+        }
+
+        val assetManager = context.assets
+        val inputStream = assetManager.open(fileName)
+        val outputStream = FileOutputStream(file)
+
+        inputStream.copyTo(outputStream)
+
+        inputStream.close()
+        outputStream.close()
+    }
+
+    private fun writeToFile(fileName: String, people: List<Person>) {
+        val file = File(requireContext().filesDir, fileName)
+        val json = gson.toJson(people)
+        file.writeText(json)
+    }
+
+    private fun readFromFile(fileName: String): MutableList<Person> {
+        val file = File(requireContext().filesDir, fileName)
+        val json = file.readText()
+        return gson.fromJson(json, object : TypeToken<MutableList<Person>>() {}.type)
+    }
+
+    private fun showAddContactDialog(fileName: String, adapter: ArrayAdapter<String>) {
+        val builder = AlertDialog.Builder(requireContext())
+        builder.setTitle("새로운 연락처를 추가하세요!")
+
+        val view = layoutInflater.inflate(R.layout.dialog_add_contact, null)
+        builder.setView(view)
+
+        builder.setPositiveButton("확인") { _, _ ->
+            val name = view.findViewById<EditText>(R.id.name).text.toString()
+            val number = view.findViewById<EditText>(R.id.number).text.toString()
+            val email = view.findViewById<EditText>(R.id.email).text.toString()
+            val instagram = view.findViewById<EditText>(R.id.instagram).text.toString()
+            val github = view.findViewById<EditText>(R.id.github).text.toString()
+
+            val newPerson = Person(name, number, email, instagram, github)
+            val people = readFromFile(fileName)
+            people.add(newPerson)
+            writeToFile(fileName, people)
+
+            adapter.clear() // 기존 데이터 제거
+            adapter.addAll(people.map { "${it.name} : ${it.number}" }) // 새로운 데이터 추가
+            adapter.notifyDataSetChanged() // ListView 갱신
+        }
+
+        builder.setNegativeButton("취소") { dialog, _ -> dialog.cancel() }
+
+        builder.show()
+    }
+
+    private fun showDetailsDialog(person: Person) {
+        val builder = AlertDialog.Builder(requireContext())
+        builder.setTitle("세부 정보")
+
+        val view = layoutInflater.inflate(R.layout.person_details_dialog, null)
+        builder.setView(view)
+
+        val nameTextView = view.findViewById<TextView>(R.id.nameTextView)
+        val numberTextView = view.findViewById<TextView>(R.id.numberTextView)
+        val emailTextView = view.findViewById<TextView>(R.id.emailTextView)
+        val instagramTextView = view.findViewById<TextView>(R.id.instagramTextView)
+        val githubTextView = view.findViewById<TextView>(R.id.githubTextView)
+
+        nameTextView.text = "이름: ${person.name}"
+        numberTextView.text = "전화번호: ${person.number}"
+        emailTextView.text = "E-mail: ${person.email}"
+        instagramTextView.text = "Instagram ID: ${person.instagram}"
+        githubTextView.text = "Github ID: ${person.github}"
+
+        builder.setPositiveButton("닫기") { dialog, _ ->
+            dialog.dismiss()
+        }
+
+        builder.show()
+    }
+
 }
