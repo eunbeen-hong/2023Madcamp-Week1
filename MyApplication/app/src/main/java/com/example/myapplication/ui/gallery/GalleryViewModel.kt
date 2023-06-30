f71444b5917b4e443b4a74bdbf9b8e875d69e9ae
frica12 <fricaa@naver.com>
2023-06-30 12:16:00 +0900

    prototype

--- MyApplication/app/src/main/java/com/example/myapplication/ui/gallery/GalleryViewModel.kt
+++ MyApplication/app/src/main/java/com/example/myapplication/ui/gallery/GalleryViewModel.kt
@@ -1,13 +1,13 @@
-package com.example.myapplication.ui.notifications
+package com.example.myapplication.ui.gallery
 
 import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
 
-class NotificationsViewModel : ViewModel() {
+class GalleryViewModel : ViewModel() {
 
     private val _text = MutableLiveData<String>().apply {
-        value = "This is notifications Fragment"
+        value = "This is Gallery Fragment"
     }
     val text: LiveData<String> = _text
 }
--- MyApplication/app/src/main/java/com/example/myapplication/ui/home/HomeFragment.kt
+++ MyApplication/app/src/main/java/com/example/myapplication/ui/home/HomeFragment.kt
@@ -1,13 +1,11 @@
 package com.example.myapplication.ui.home
 
 import android.os.Bundle
-import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.ArrayAdapter
 import android.widget.ListView
-import android.widget.TextView
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.ViewModelProvider
 import com.example.myapplication.databinding.FragmentHomeBinding
